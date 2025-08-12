fe6c9a95d7e4c78c29c26a3c85520ac6316e6004
bvandercar-vt <blake.vandercar@validatek.com>
2025-08-12 23:00:00 +0000

    try Masonry layout

--- src/components/GridBody.tsx
+++ src/components/GridBody.tsx
@@ -1,103 +1,102 @@
 import { faExternalLink } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { useMemo } from 'react'
+import Masonry from 'react-masonry-css'
 import { getIsMobile } from '../utils/html-utils'
 import type { GridCardProps } from './GridCard'
 import { GridCard, GridCardsProvider, useGridCards } from './GridCard'
 import { SoundcloudTrack } from './SoundcloudTrack'
 
-const GridCards = ({ initiallyOpen }: Pick<GridCardProps, 'initiallyOpen'>) => (
-    <>
-      <GridCard title="Wave / Downtempo / Psydub" initiallyOpen={initiallyOpen}>
-        <SoundcloudTrack
-          url="https://soundcloud.com/special-ingredient/plant-life-vol-1?in=special-ingredient/sets/trippy-melty-wavy-bass"
-          title="Plant Life Vol. 1"
-          subTitle="Psydub & Downtempo Bass Journey"
-        />
-        <SoundcloudTrack
-          url="https://soundcloud.com/special-ingredient/bass-canyon-late-night-trippy-set-pt-1-back-2-camp?in=special-ingredient/sets/trippy-melty-wavy-bass"
-          title="Late Night Set @ Bass Canyon Pt. 1"
-          subTitle="Trippy Genre Journey - Wave, Psydub, Downtempo, etc"
-        />
-        <SoundcloudTrack
-          url="https://soundcloud.com/special-ingredient/bass-canyon-late-night-trippy-set-pt-2?in=special-ingredient/sets/trippy-melty-wavy-bass"
-          title="Late Night Set @ Bass Canyon Pt. 2"
-          subTitle="Trippy Genre Journey - Wave, Psydub, Downtempo, etc"
-        />
-        <SoundcloudTrack
-          url="https://soundcloud.com/special-ingredient/plant-life-vol-2?in=special-ingredient/sets/trippy-melty-wavy-bass"
-          title="Plant Life Vol. 2"
-          subTitle="Halftime Psy-Hop & Uptempo Tribal Bass"
-        />
-      </GridCard>
-      <GridCard title="Wompy Dubstep" initiallyOpen={initiallyOpen}>
-        <SoundcloudTrack
-          url="https://soundcloud.com/special-ingredient/subwoofer-sauce-vol-1?in=special-ingredient/sets/heavier-dubstep"
-          title="Subwoofer Sauce Vol. 1"
-          subTitle="OG Late Night Wonky Riddim"
-          additionalInfo={
-            <>
-              <i>True</i> riddim, carefully picked. If you're not a fan of riddim yet, try this one
-            out. Headphones recommended, Lotta wild sounds bouncing around the 3D space in this one.
-            </>
-          }
-        />
-        <SoundcloudTrack
-          url="https://soundcloud.com/special-ingredient/aged-cheddar-mix-vol-1?in=special-ingredient/sets/heavier-dubstep"
-          title="Aged Cheddar Vol. 1"
-          subTitle="Early 2010s Wompy Dubstep— Liquid Stranger, Bar9, etc"
-        />
-      </GridCard>
-      <GridCard title="Hype Dubstep" initiallyOpen={initiallyOpen}>
-        <SoundcloudTrack
-          url="https://soundcloud.com/special-ingredient/live-set-dft-yellow-brick-road-tour?in=special-ingredient/sets/heavier-dubstep"
-          title="Live Set @ DFT Yellow Brick Road Tour"
-          subTitle="Hype Dubstep Journey"
-          additionalInfo={
-            <>
-              Threw down more of a hype set for Dancefestopia's Yellow Brick Road tour, while still
-              tryna keep it a genre journey. Lotta bangers but we still get swampy and wavey :)
-            </>
-          }
-        />
-        <SoundcloudTrack
-          url="https://soundcloud.com/special-ingredient/special-ingredient-b2b-saumii-larimer-lounge-1-26-2023?in=special-ingredient/sets/heavier-dubstep"
-          title="Special Ingredient B2B Saumii @ Larimer Lounge"
-          subTitle="Hype Dubstep Journey"
-          additionalInfo={
-            <>
-              Cooked up an extra special bass journey set with{' '}
-              <a href="https://soundcloud.com/saumiimusic" target="__blank">
-                Saumii <FontAwesomeIcon icon={faExternalLink} size="2xs" />
-              </a>{' '}
-            for a sold out crowd at Larimer Lounge, rinsing some of our favorite tracks we've shared
-            over the years of DJing together.
-            </>
-          }
-        />
-        <SoundcloudTrack
-          url="https://soundcloud.com/special-ingredient/bass-kitchen-mix-rage-dubstep-mix-briddim-riddim?in=special-ingredient/sets/heavier-dubstep"
-          title="Bass Kitchen Vol. 1"
-          subTitle="Briddim, Riddim, Heavy Dubstep"
-        />
-      </GridCard>
-      <GridCard title="Mashups / Flips" initiallyOpen={initiallyOpen}>
-        {[
-          'https://soundcloud.com/special-ingredient/meduso-a-moment-vip-x-baby-bash-cyclone?in=special-ingredient/sets/mashups-flips',
-          'https://soundcloud.com/special-ingredient/skeler-x-jojo-x-blackstreet-no-diggity-mashup?in=special-ingredient/sets/mashups-flips',
-          'https://soundcloud.com/special-ingredient/akon-love-right-now-na-na-na-x-nit-grit-mashup?in=special-ingredient/sets/mashups-flips',
-          'https://soundcloud.com/special-ingredient/ian-snow-revelation-x-travis-scott-kid-cudi-through-the-late-night?in=special-ingredient/sets/mashups-flips',
-        ].map((url, index) => (
-          <SoundcloudTrack url={url} albumArtToSide key={`mashup-track-${index}`} />
-        ))}
-      </GridCard>
-      {/* <GridCard title="Spotify Playlists">
+const GridCards = ({ initiallyOpen }: Pick<GridCardProps, 'initiallyOpen'>) => [
+  <GridCard title="Wave / Downtempo / Psydub" initiallyOpen={initiallyOpen}>
+    <SoundcloudTrack
+      url="https://soundcloud.com/special-ingredient/plant-life-vol-1?in=special-ingredient/sets/trippy-melty-wavy-bass"
+      title="Plant Life Vol. 1"
+      subTitle="Psydub & Downtempo Bass Journey"
+    />
+    <SoundcloudTrack
+      url="https://soundcloud.com/special-ingredient/bass-canyon-late-night-trippy-set-pt-1-back-2-camp?in=special-ingredient/sets/trippy-melty-wavy-bass"
+      title="Late Night Set @ Bass Canyon Pt. 1"
+      subTitle="Trippy Genre Journey - Wave, Psydub, Downtempo, etc"
+    />
+    <SoundcloudTrack
+      url="https://soundcloud.com/special-ingredient/bass-canyon-late-night-trippy-set-pt-2?in=special-ingredient/sets/trippy-melty-wavy-bass"
+      title="Late Night Set @ Bass Canyon Pt. 2"
+      subTitle="Trippy Genre Journey - Wave, Psydub, Downtempo, etc"
+    />
+    <SoundcloudTrack
+      url="https://soundcloud.com/special-ingredient/plant-life-vol-2?in=special-ingredient/sets/trippy-melty-wavy-bass"
+      title="Plant Life Vol. 2"
+      subTitle="Halftime Psy-Hop & Uptempo Tribal Bass"
+    />
+  </GridCard>,
+  <GridCard title="Wompy Dubstep" initiallyOpen={initiallyOpen}>
+    <SoundcloudTrack
+      url="https://soundcloud.com/special-ingredient/subwoofer-sauce-vol-1?in=special-ingredient/sets/heavier-dubstep"
+      title="Subwoofer Sauce Vol. 1"
+      subTitle="OG Late Night Wonky Riddim"
+      additionalInfo={
+        <>
+          <i>True</i> riddim, carefully picked. If you're not a fan of riddim yet, try this one out.
+          Headphones recommended, Lotta wild sounds bouncing around the 3D space in this one.
+        </>
+      }
+    />
+    <SoundcloudTrack
+      url="https://soundcloud.com/special-ingredient/aged-cheddar-mix-vol-1?in=special-ingredient/sets/heavier-dubstep"
+      title="Aged Cheddar Vol. 1"
+      subTitle="Early 2010s Wompy Dubstep— Liquid Stranger, Bar9, etc"
+    />
+  </GridCard>,
+  <GridCard title="Hype Dubstep" initiallyOpen={initiallyOpen}>
+    <SoundcloudTrack
+      url="https://soundcloud.com/special-ingredient/live-set-dft-yellow-brick-road-tour?in=special-ingredient/sets/heavier-dubstep"
+      title="Live Set @ DFT Yellow Brick Road Tour"
+      subTitle="Hype Dubstep Journey"
+      additionalInfo={
+        <>
+          Threw down more of a hype set for Dancefestopia's Yellow Brick Road tour, while still
+          tryna keep it a genre journey. Lotta bangers but we still get swampy and wavey :)
+        </>
+      }
+    />
+    <SoundcloudTrack
+      url="https://soundcloud.com/special-ingredient/special-ingredient-b2b-saumii-larimer-lounge-1-26-2023?in=special-ingredient/sets/heavier-dubstep"
+      title="Special Ingredient B2B Saumii @ Larimer Lounge"
+      subTitle="Hype Dubstep Journey"
+      additionalInfo={
+        <>
+          Cooked up an extra special bass journey set with{' '}
+          <a href="https://soundcloud.com/saumiimusic" target="__blank">
+            Saumii <FontAwesomeIcon icon={faExternalLink} size="2xs" />
+          </a>{' '}
+          for a sold out crowd at Larimer Lounge, rinsing some of our favorite tracks we've shared
+          over the years of DJing together.
+        </>
+      }
+    />
+    <SoundcloudTrack
+      url="https://soundcloud.com/special-ingredient/bass-kitchen-mix-rage-dubstep-mix-briddim-riddim?in=special-ingredient/sets/heavier-dubstep"
+      title="Bass Kitchen Vol. 1"
+      subTitle="Briddim, Riddim, Heavy Dubstep"
+    />
+  </GridCard>,
+  <GridCard title="Mashups / Flips" initiallyOpen={initiallyOpen}>
+    {[
+      'https://soundcloud.com/special-ingredient/meduso-a-moment-vip-x-baby-bash-cyclone?in=special-ingredient/sets/mashups-flips',
+      'https://soundcloud.com/special-ingredient/skeler-x-jojo-x-blackstreet-no-diggity-mashup?in=special-ingredient/sets/mashups-flips',
+      'https://soundcloud.com/special-ingredient/akon-love-right-now-na-na-na-x-nit-grit-mashup?in=special-ingredient/sets/mashups-flips',
+      'https://soundcloud.com/special-ingredient/ian-snow-revelation-x-travis-scott-kid-cudi-through-the-late-night?in=special-ingredient/sets/mashups-flips',
+    ].map((url, index) => (
+      <SoundcloudTrack url={url} albumArtToSide key={`mashup-track-${index}`} />
+    ))}
+  </GridCard>,
+  /* <GridCard title="Spotify Playlists">
 <div className="collapse-content">
 <div className="playlist-folder-tree"></div>
 </div>
-</GridCard>  */}
-    </>
-  )
+</GridCard>  */
+]
 
 const GridCardsWrapper = () => {
   const { openIds, allIds, expandingRef } = useGridCards()
@@ -117,9 +116,19 @@ const GridCardsWrapper = () => {
           Click for SoundCloud mixes! <span className="fa fa-arrow-down" />
         </span>
       )}
-      <div id="main-grid">
-        <GridCards initiallyOpen={!getIsMobile()} />
-      </div>
+      <Masonry
+        breakpointCols={{
+          default: 5,
+          2250: 4,
+          1800: 3,
+          1350: 2,
+          900: 1,
+        }}
+        columnClassName="masonry-grid-column"
+        className="main-masonry-grid"
+      >
+        {GridCards({ initiallyOpen: !getIsMobile() })}
+      </Masonry>
       {
         // create a div at the bottom when expanding so can scroll title to the top
         expandingRef?.current && openIds.length === 0 && <div style={{ height: spacerHeight }} />
