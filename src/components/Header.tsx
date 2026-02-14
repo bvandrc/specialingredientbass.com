ad5b57ba1c11c9b56acd6235ed0b6ff502105c11
Blake Vandercar <blake.vandercar@validatek.com>
2026-02-13 21:37:43 -0700

    header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,6 +7,13 @@ import {
 } from '@fortawesome/free-brands-svg-icons'
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
 import { CircleLink } from './primitives/CircleLink'
+import classNames from 'classnames'
+
+const SOCIAL_LINK = classNames(
+  'size-11 text-2xl',
+  '[background-image:linear-gradient(rgb(0_0_0/30%)_0_0)]',
+  'hover:[background-image:none] hover:shadow-[0_0_20px_yellow]',
+)
 
 export const Header = () => {
   return (
@@ -18,8 +25,10 @@ export const Header = () => {
           id="header-logo-img"
         />
       </div>
-      <div id="header-right">
-        <section id="bio" aria-label="bio">
+      <div
+        className="relative mx-auto text-center lg:absolute lg:right-5 lg:bottom-0 lg:text-right"
+      >
+        <section aria-label="bio" className="my-2 text-gray-500 font-[Outfit,sans-serif] leading-5">
           All forms of bass music 🌀
           <br />
           Tracklist in every mix 🎵
@@ -33,39 +42,43 @@ export const Header = () => {
           AKA DJ Smoothbrain 😉
         </section>
         {/** biome-ignore lint/a11y/useSemanticElements: is fine as Div */}
-        <div id="social-links" role="group" aria-label="social links">
+        <div
+          role="group"
+          aria-label="social links"
+          className="flex justify-center items-center gap-1 lg:justify-end"
+        >
           <CircleLink
-            className="soundcloud"
+            className={classNames(SOCIAL_LINK, 'soundcloud')}
             title="SoundCloud"
             icon={faSoundcloud}
             href="https://www.soundcloud.com/special-ingredient"
           />
           <CircleLink
-            className="twitter"
+            className={classNames(SOCIAL_LINK, 'twitter')}
             title="Twitter"
             icon={faTwitter}
             href="https://www.twitter.com/dj_smoothbrain"
           />
           <CircleLink
-            className="instagram"
+            className={classNames(SOCIAL_LINK, 'instagram')}
             title="Instagram"
             icon={faInstagram}
             href="https://www.instagram.com/special_ingredient_bass"
           />
           <CircleLink
-            className="reddit"
+            className={classNames(SOCIAL_LINK, 'reddit')}
             title="Reddit"
             icon={faReddit}
             href="https://www.reddit.com/user/SpecialIngredient"
           />
           <CircleLink
-            className="facebook"
+            className={classNames(SOCIAL_LINK, 'facebook')}
             title="Facebook"
             icon={faFacebook}
             href="https://www.facebook.com/profile.php?id=100087612335247"
           />
           <CircleLink
-            className="email"
+            className={classNames(SOCIAL_LINK, 'email')}
             title="Email"
             icon={faEnvelope}
             onClick={() => {
