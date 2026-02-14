108f034d33eef96012e6c52799ecf2faff139e33
Blake Vandercar <blake.vandercar@validatek.com>
2026-02-14 00:28:33 -0700

    remove Tree features, add later from an external package

--- src/components/primitives/FolderTree.tsx
+++
@@ -1,6 +0,0 @@
-import classNames from 'classnames'
-import { Tree, type TreeProps } from './Tree'
-
-export const FolderTree = ({ nodes, className }: TreeProps) => (
-  <Tree className={classNames('folder-tree', className)} nodes={nodes} />
-)
